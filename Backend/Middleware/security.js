2f3cef31b561df0dc1b6ed65f8f466ec807c8a2f
Ushikunde1255 <Ushikunde@gmail.com>
2026-07-20 13:50:42 +0000

    Delete Backend directory

--- Backend/Middleware/security.js
+++
@@ -1,5 +0,0 @@
-const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
-exports.helmet = helmet();
-exports.generalLimiter = rateLimit({ windowMs: 60*1000, max: 200 });
-exports.authLimiter = rateLimit({ windowMs: 15*60*1000, max: 20 });
