15979512e8d206ea40315a1f6cd5078da80bc8de
Ushikunde1255 <Ushikunde@gmail.com>
2026-07-28 17:27:12 +0000

    Update job.js

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -13,7 +13,7 @@ cloudinary.config({
 
 const upload = multer({
   storage: multer.memoryStorage(),
-  limits: { fileSize: 5 * 1024 }
+  limits: { fileSize: 5 * 1024 * 1024}
 });
 
 router.get('/', async (req, res) => {
